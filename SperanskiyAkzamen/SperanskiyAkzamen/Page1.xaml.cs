e5f9fd5e707d2d5e4a7f38e9b259dfa4923dd25c
gruppa203 <gruppa203git@mail.ru>
2023-12-08 16:14:51 +0300

    1

---
+++ SperanskiyAkzamen/SperanskiyAkzamen/Page1.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SperanskiyAkzamen
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Voiti_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var userObj = Bd_SperanskiyEntities.GetContext().Users.FirstOrDefault(x => x.Login == txbLogin.Text && x.Password == PbPassword.Password);
+                if (userObj == null)
+                {
+                    MessageBox.Show("Пользователь с таким логином и паролем найден!", "Ошибка входа", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    AppFrame.MainFrame.Navigate(new Page2());
+                }
+
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show("Ошибка:" + Ex.Message.ToString(), "Критическая работа приложения", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+    }
+}
+        
+    
